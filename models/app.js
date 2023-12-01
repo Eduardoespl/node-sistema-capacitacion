9ec50de33c4942198b57fedc80aa4a82bbc48e2f
Eduardoespl <eduardoesp3317@gmail.com>
2023-12-01 00:47:25 -0600

    created models folder and added app.js file

---
+++ models/app.js
@@ -0,0 +1,39 @@
+//const http = require('http');
+const express = require('express');
+const app = express();
+const puerto = 80;
+
+const TrabajadoresController = require('./controllers/trabajadoresController');
+app.use(express.json());
+app.get('/trabajador', TrabajadoresController.indexGet);
+app.get('/trabajador/:id([0-9]+)', TrabajadoresController.getTrabajadorById);
+
+const CursosController = require('./controllers/cursosController');
+app.use(express.json());
+app.get('/cursos', CursosController.indexGet);
+
+const CategoriasController = require('./controllers/categoriasController');
+app.use(express.json());
+app.get('/categorias', CategoriasController.indexGet);
+
+const PuestosController = require('./controllers/puestosController');
+app.use(express.json());
+app.get('/puestos', PuestosController.indexGet);
+
+const VacantesController = require('./controllers/vacantesController');
+app.use(express.json());
+app.get('/vacantes', VacantesController.indexGet);
+
+const Trabajadores_CursosController = require('./controllers/trabajadores_cursosController');
+app.use(express.json());
+app.get('/trabajadores_cursos', Trabajadores_CursosController.indexGet);
+
+app.get('/', function(req, res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hola Mundo!');
+});
+
+app.listen(puerto, function() {
+    console.log("Servidor iniciado");
+});
