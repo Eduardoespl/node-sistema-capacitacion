b6069487771b74ab7527f94085f56b47195662c2
Eduardoespl <eduardoesp3317@gmail.com>
2023-12-01 00:41:01 -0600

    created database.sql file

---
+++ database.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE IF NOT EXISTS capacitacion;
+
+USE capacitacion;
+
+CREATE TABLE trabajadores (
+    id_trabajador INTEGER NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    fk_id_puesto INTEGER NOT NULL,
+    cursos_completos INTEGER NOT NULL,
+    PRIMARY KEY (id_trabajador),
+    FOREIGN KEY (fk_id_puesto) REFERENCES puestos(id_puesto)
+)
+
+CREATE TABLE cursos (
+    id_curso INTEGER NOT NULL AUTO_INCREMENT,
+    fk_id_categoria INTEGER NOT NULL,
+    nombre TIME NOT NULL,
+    aptitud TIME NOT NULL,
+    PRIMARY KEY (id_curso),
+    FOREIGN KEY (fk_id_categoria) REFERENCES categorias(id_categoria)
+)
+
+CREATE TABLE categorias (
+    id_categoria INTEGER NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id_categoria)
+)
+
+CREATE TABLE puestos (
+    id_puesto INTEGER NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id_puesto)
+)
+
+CREATE TABLE vacantes (
+    id_vacante INTEGER NOT NULL AUTO_INCREMENT,
+    fk_id_puesto INTEGER NOT NULL,
+    fk_id_curso INTEGER NOT NULL,
+    PRIMARY KEY (id_vacante),
+    FOREIGN KEY (fk_id_puesto) REFERENCES puestos(id_puesto),
+    FOREIGN KEY (fk_id_curso) REFERENCES cursos(id_curso)
+)
+
+CREATE TABLE trabajadores_cursos (
+    id_trabajador_curso INTEGER NOT NULL AUTO_INCREMENT,
+    fk_id_trabajador INTEGER NOT NULL,
+    fk_id_curso INTEGER NOT NULL,
+    PRIMARY KEY (id_trabajador_curso),
+    FOREIGN KEY (fk_id_trabajador) REFERENCES trabajadores(id_trabajador),
+    FOREIGN KEY (fk_id_curso) REFERENCES cursos(id_curso)
+)
